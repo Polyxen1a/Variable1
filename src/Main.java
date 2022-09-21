30940052157831858798756dafbade3b3b584dd2
Polyxen1a <maslowa@inbox.ru>
2022-09-21 17:36:50 +0300

    Variable

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,47 @@ public class Main {
         System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
+
+        var friend = 19.0;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var oneFighter = 78.2;
+        var twoFighter = 82.7;
+        var fightersWeight = oneFighter + twoFighter;
+        System.out.println("Общий вес бойцов " + fightersWeight + " кг");
+
+        var differenceWeight = twoFighter - oneFighter;
+        System.out.println("Разница бойцов в весе " + differenceWeight + " кг");
+
+        var remains = twoFighter % oneFighter;
+        System.out.println("Остаток деления " + remains + " кг");
+
+        var hours = 640;
+        var work = 8;
+        var staff = hours / work;
+        System.out.println("Всего работников в компании – " + staff + " человек");
+
+        var newWork = 8;
+        var newStaff = staff + 94;
+        System.out.println("Всего сотрудников в компании " + newStaff + " человек");
+        var officeHours = newWork * newStaff;
+        System.out.println("Если в компании работает " + newStaff + " человека, то всего " + officeHours + " часа работы может быть поделено между сотрудниками");
+
+
+
+
     }
 }
